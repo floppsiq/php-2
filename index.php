d753025b0db0d41d63a13006a433a1223372d0b5
floppsiq <krinicinda@gmail.com>
2021-12-14 23:42:47 +0700

    completed lesson-2

--- index.php
+++ index.php
@@ -1,71 +1,54 @@
 <?php 
-    class Product {
-        public $id;
-        public $name;
-        public $brand;
-        public $category;
-        public $price;
-        public $count;
+    abstract class Product {
 
-        public function __construct($id, $category, $name, $brand, $price, $count)
-        {
-            $this->id = $id;
-            $this->category = $category;
-            $this->name = $name;
-            $this->brand = $brand;
-            $this->price = $price;
-            $this->count = $count;
+        protected static $cost = 30;
+    
+        abstract public function getFinalCost();
+    }
+    
+    class DigitalProduct extends Product {
+    
+        public function getFinalCost() {
+            return self::$cost/2;
         }
-
-        public function getProduct()
-        {
-            echo "<hr><h2>Карточка товара</h2>
-            <b>Категория:</b> $this->category<br>
-            <b>Наименование:</b> $this->name<br>
-            <b>Бренд:</b> $this->brand<br>
-            <b>Цена:</b> $this->price руб.<br>
-            <b>Количество на складе:</b> $this->count шт.<br>";
+    
+    }
+    
+    class PeiceProduct extends Product {
+    
+        public function getFinalCost() {
+            return self::$cost;
         }
-
-        public function getPrice() {
-            return $this->price;
+    
+    }
+    
+    class WeightProduct extends Product {
+        private $qty;
+    
+        public function __construct() {
+            $this->qty = 0;
         }
-
-        public function setPrice(int $newPrice) {
-            $this->price = $newPrice;
+    
+        public function setQty($qty) {
+            $this->qty = $qty;
+        }
+    
+        public function getQty($qty) {
+            $this->qty = $qty;
+        }
+    
+        public function getFinalCost() {
+            return $this->qty * self::$cost;
         }
     }
-
-    class promoProduct extends Product {
-
-        public $discount;
-        public $sale;
-
-        public function __construct($id, $category, $name, $brand, $price, $count, $discount, $sale)
-        {
-            parent::__construct($id, $category, $name, $brand, $price, $count);
-            $this->discount = $discount;
-            $this->sale = $sale;
-        }
-
-        public function getProduct()
-        {
-            parent::getProduct();
-            echo "<b>Скидка:</b> $this->discount%<br>
-            <b>Акция:</b> $this->sale<br>";
-        }
-
-        public function getSale() {
-            return ($this->price) - ($this->price*$this->discount/100);
-        }
-    }
-
-    $good1 = new Product(1, "ЦП", "Intel Core i3-7100T", "INTEL", 13000, 7);
-    $good1->getProduct();
-    echo "Стоимость товара " . $good1->getPrice(). " руб";
-    $good1->setPrice(14000);
-    echo "<br>Новая стоимость товара " . $good1->getPrice(). " руб";
-    $good2 = new promoProduct(1, "ЦП", "Intel Core i5-9400F", "INTEL", 18087, 10, 25, "Черная пятница");
-    $good2->getProduct();
-    echo "Стоимость товара " . $good2->getPrice(). " руб<br>Со скидкой: " . $good2->getSale();
+    
+    $prod1 = new DigitalProduct();
+    $prod2 = new PeiceProduct();
+    $prod3 = new WeightProduct();
+    
+    $prod3->setQty(3);
+    
+    echo "Стоимость цифрового товара {$prod1->getFinalCost()} <br/>";
+    echo "Стоимость штучного товара {$prod2->getFinalCost()} <br/>";
+    echo "Стоимость весового товара {$prod3->getFinalCost()} <br/>"; 
 ?>
